c302af94c9f62569071baf92a931ba8f5b4ab70d
Kümmel Christian <c.kuemmel@microstream.one>
2021-10-04 09:36:23 +0200

    zc

--- src/main/java/one/microstream/training/s3/storage/DB.java
+++ src/main/java/one/microstream/training/s3/storage/DB.java
@@ -5,12 +5,8 @@ import java.util.Optional;
 
 import io.micronaut.core.io.ResourceResolver;
 import io.micronaut.core.io.scan.ClassPathResourceLoader;
-import one.microstream.enterprise.afs.aws.s3.types.S3Connector;
-import one.microstream.enterprise.afs.blobstore.types.BlobStoreFileSystem;
 import one.microstream.storage.embedded.configuration.types.EmbeddedStorageConfiguration;
 import one.microstream.storage.embedded.types.EmbeddedStorageManager;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
 
 
 public class DB
@@ -24,17 +20,8 @@ public class DB
 		Optional<URL> resource = loader.getResource("microstream.xml");
 		
 		System.setProperty("aws.region", "eu-central-1");
-		
-		S3Client client = S3Client.builder().region(Region.EU_CENTRAL_1).build();
-		
-		BlobStoreFileSystem fileSystem = BlobStoreFileSystem.New(
-			S3Connector.Caching(client)
-		);
-		
 		storageManager = EmbeddedStorageConfiguration.load(
 			resource.get().getPath()).createEmbeddedStorageFoundation().createEmbeddedStorageManager(root).start();
-		
-		
 	}
 	
 }
