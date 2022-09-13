71d8cf194844e924425d0755f1b3c7c870aa52d2
Kümmel Christian <c.kuemmel@microstream.one>
2022-09-13 10:35:54 +0200

    AWS API upgrade

--- src/main/java/one/microstream/training/s3/storage/DB.java
+++ src/main/java/one/microstream/training/s3/storage/DB.java
@@ -1,12 +1,12 @@
 package one.microstream.training.s3.storage;
 
-import java.net.URL;
-import java.util.Optional;
-
-import io.micronaut.core.io.ResourceResolver;
-import io.micronaut.core.io.scan.ClassPathResourceLoader;
-import one.microstream.storage.embedded.configuration.types.EmbeddedStorageConfiguration;
+import one.microstream.enterprise.afs.aws.s3.types.S3Connector;
+import one.microstream.enterprise.afs.blobstore.types.BlobStoreFileSystem;
+import one.microstream.storage.embedded.types.EmbeddedStorage;
 import one.microstream.storage.embedded.types.EmbeddedStorageManager;
+import software.amazon.awssdk.auth.credentials.SystemPropertyCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 
 
 public class DB
@@ -16,12 +16,28 @@ public class DB
 	
 	static
 	{
-		ClassPathResourceLoader loader = new ResourceResolver().getLoader(ClassPathResourceLoader.class).get();
-		Optional<URL> resource = loader.getResource("microstream.xml");
+//		System.setProperty("aws.accessKeyId", "enter access key here");
+//		System.setProperty("aws.secretAccessKey", "enter secret key here");
 		
-		System.setProperty("aws.region", "eu-central-1");
-		storageManager = EmbeddedStorageConfiguration.load(
-			resource.get().getPath()).createEmbeddedStorageFoundation().createEmbeddedStorageManager(root).start();
+		SystemPropertyCredentialsProvider credentialsProvider = SystemPropertyCredentialsProvider.create();
+
+        Region region = Region.EU_CENTRAL_1;
+        S3Client s3 = S3Client.builder()
+            .region(region)
+            .credentialsProvider(credentialsProvider)
+            .build();
+		
+//		Just to check if nativ AWS connection is working
+//        ListBucketsRequest listBucketsRequest = ListBucketsRequest.builder().build();
+//        ListBucketsResponse listBucketsResponse = s3.listBuckets(listBucketsRequest);
+//        listBucketsResponse.buckets().stream().forEach(x -> System.out.println(x.name()));
+                
+        BlobStoreFileSystem fileSystem = BlobStoreFileSystem.New(
+        	S3Connector.Caching(s3)
+        );
+        
+        String S3bucketname = "microstreamstoragedemo";
+        storageManager = EmbeddedStorage.start(fileSystem.ensureDirectoryPath(S3bucketname));
 	}
 	
 }
